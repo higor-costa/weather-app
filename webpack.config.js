ae49ad48e5671ef6a3cceff9563962971afaefd2
Higor Costa <higorcosta972@gmail.com>
2025-09-09 18:40:35 -0300

    chore(webpack): altera os caminhos dos arquivos de entrada e saída

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['./js/script.js'],
+  entry: ['./assets/js/script.js'],
   output: {
-    path: path.resolve(__dirname, './'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'main.js'
   }
 }
