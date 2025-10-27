cf98ee89788597f1df8180005b64c386b2be4306
Higor Costa <higorcosta972@gmail.com>
2025-10-27 18:18:33 -0300

    chore(webpack.config.js): usa import ao invés de require

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: ['./assets/js/script.js'],
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'main.js'
-  }
-}
+    filename: 'main.js',
+  },
+};
