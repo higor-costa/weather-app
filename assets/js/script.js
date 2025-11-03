f958d25fa8f8f9000b2ca2d66b7622ee01b02f5a
Higor Costa <higorcosta972@gmail.com>
2025-11-03 19:31:38 -0300

    feat(script.js): importa e inicializa função
    Importa e inicializa a função initHourForecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 import initDropdownButtons from './modules/dropdowns.js';
 import initSelectUnitsMeasures from './modules/selectUnitsMeasures.js';
+import initHourForecast from './modules/hoursForecast.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initDropdownButtons();
   initSelectUnitsMeasures();
+  initHourForecast();
 });
