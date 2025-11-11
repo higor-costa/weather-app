82ad59ec0de231fa7cbc1735cdaf47891bd19b8e
Higor Costa <higorcosta972@gmail.com>
2025-11-11 19:30:03 -0300

    feat(scrip.js): importa e inicializa funções

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 import initDropdownButtons from './modules/dropdowns.js';
 import initSelectUnitsMeasures from './modules/selectUnitsMeasures.js';
 import initHourForecast from './modules/hoursForecast.js';
+import initLocalCoords from './modules/localCoords.js';
+import initLocalForecast from './modules/localForecast.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initDropdownButtons();
   initSelectUnitsMeasures();
   initHourForecast();
+  initLocalCoords();
+  initLocalForecast();
 });
