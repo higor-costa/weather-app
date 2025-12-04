017703b683b8a8c2798b70653a3eee42db701e8b
Higor Costa <higorcosta972@gmail.com>
2025-12-04 16:36:40 -0300

    feat(script.js): inicializa função
    Inicializa a função initSearchCityForecast.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@ import initDropdownButtons from './modules/dropdowns.js';
 import initSelectUnitsMeasures from './modules/selectUnitsMeasures.js';
 import initHourForecast from './modules/hoursForecast.js';
 import initLocalCoords from './modules/localCoords.js';
+import initSearchCityForecast from './modules/searchCityForecast.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initDropdownButtons();
   initSelectUnitsMeasures();
   initHourForecast();
   initLocalCoords();
+  initSearchCityForecast();
 });
