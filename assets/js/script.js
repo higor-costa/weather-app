84dc97b0392f405131b88c4620dec56852f56134
Higor Costa <higorcosta972@gmail.com>
2026-01-02 20:50:54 -0300

    feat(script.js): remove importação da função initHourForecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 import initDropdownButtons from './modules/dropdowns.js';
 import initSelectUnitsMeasures from './modules/selectUnitsMeasures.js';
-import initHourForecast from './modules/hoursForecast.js';
 import initLocalCoords from './modules/localCoords.js';
 import initSearchCityForecast from './modules/searchCityForecast.js';
 import initLocalStorage from './modules/localStorage.js';
@@ -8,7 +7,6 @@ import initLocalStorage from './modules/localStorage.js';
 document.addEventListener('DOMContentLoaded', () => {
   initDropdownButtons();
   initSelectUnitsMeasures();
-  initHourForecast();
   initLocalCoords();
   initSearchCityForecast();
   initLocalStorage();
